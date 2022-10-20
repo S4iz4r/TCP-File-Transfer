ef59ee0b4be7915f380e295b51194280f6a8815e
S4iz4r <85998902+S4iz4r@users.noreply.github.com>
2022-10-20 12:48:24 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,247 @@
+import socket
+import tqdm
+import os
+import threading
+import platform
+import re
+from threading import Thread
+from hashlib import sha256
+
+PORT = 4456
+IP = '127.0.0.1'
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+SELF_IP = s.getsockname()[0]
+s.close()
+FORMAT = "utf-8"
+SIZE = 8192  # 1024
+CLIENT_DATA_PATH = 'downloaded'
+
+platform = platform.system()
+re_ip = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
+
+delete_file = 'rm'if platform != 'Windows' else 'del'
+slash = '/'if platform != 'Windows' else '\\'
+
+
+class CustomThread(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args, **self._kwargs)
+
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+
+
+def upload_file(fname, ip, port):
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((ip, port + 1))
+        file = open(fname, 'rb')
+        file_size = os.path.getsize(fname)
+        file_hash = sha256(open(fname, 'rb').read()).hexdigest()
+        client.send(fname.split(slash)[-1].encode())
+        client.recv(1024).decode()
+        client.send(str(file_size).encode())
+        client.recv(1024).decode()
+        client.send(str(file_hash).encode())
+        client.recv(1024).decode()
+        data = file.read()
+        client.sendall(data)
+        client.send(b'<END>')
+        file.close()
+        client.close()
+    except Exception as e:
+        print(e)
+
+
+def recive_file(ip, port, client_data_path=CLIENT_DATA_PATH):
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((ip, port + 1))
+        server.listen()
+        client, addr = server.accept()
+        file_name = client.recv(1024).decode()
+        client.send('ok'.encode())
+        file_size = client.recv(1024).decode()
+        client.send('ok'.encode())
+        file_hash = client.recv(1024).decode()
+        client.send('ok'.encode())
+        print(
+            f'Reciving file: {file_name}  Size: {file_size}\nsha256: {file_hash}\n')
+        if os.path.isdir(client_data_path) == False:
+            os.mkdir(client_data_path)
+        file = open(f'{client_data_path}{slash}' + file_name, 'wb')
+        file_bytes = b''
+        done = False
+        progress = tqdm.tqdm(unit='B', unit_scale=True,
+                             unit_divisor=1000, total=int(file_size))
+        while not done:
+            # 1048576, 1024, 8196, 65536, 1024000...
+            data = client.recv(1024000)
+            if file_bytes[-5:] == b'<END>':
+                done = True
+            else:
+                file_bytes += data
+            progress.update(len(data))
+        file.write(file_bytes.replace(b'<END>', b''))
+        file.close()
+        client.close()
+        server.close()
+        return file_name, file_hash
+    except Exception as e:
+        print(e)
+
+
+def main(IP):
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((IP, PORT))
+        client.send(platform.encode())
+    except:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        IP = s.getsockname()[0]
+        s.close()
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((IP, PORT))
+        client.send(platform.encode())
+    print(f"Connecting to [{IP}:{PORT}]")
+    while True:
+        data = client.recv(SIZE).decode()
+        try:
+            cmd, msg = data.split("@", 1)
+        except Exception as e:
+            print(e)
+
+        if cmd == "DISCONNECTED":
+            print(f"[SERVER]: {msg}")
+            break
+        elif cmd == "OK":
+            print(f"{msg}")
+        data = ''
+        while data == '':
+            try:
+                data = input("> ")
+            except:
+                print('incorrect command!')
+        data = data.split(" ")
+        cmd = data[0].lower()
+        payload = " ".join(data[1:])
+        if cmd == "help" or cmd == "-h":
+            client.send(cmd.encode())
+        elif cmd == "pwd" or cmd == 'cd':
+            client.send(cmd.encode())
+        elif cmd == "logout" or cmd == "exit" or cmd == "quit" or cmd == "q":
+            client.send(cmd.encode())
+            break
+        elif cmd == "list" or cmd == "-l" or cmd == "ls" or cmd == "dir":
+            client.send(cmd.encode())
+        elif cmd == "delete" or cmd == "rm" or cmd == "del":
+            try:
+                client.send(f"{cmd}@{payload}".encode())
+            except:
+                client.send(cmd.encode())
+        elif cmd == 'cat' or cmd == "type":
+            try:
+                client.send(f"{cmd}@{payload}".encode())
+            except:
+                client.send(cmd.encode())
+        elif cmd == "upload" or cmd == "-U" or cmd == "-u":
+            for file in payload.split(", "):
+                if os.path.isfile(file):
+                    try:
+                        client.send(f'{cmd}@{file}'.encode())
+                        threading.Thread(target=upload_file, args=(
+                            file, IP, PORT,)).start()
+                    except:
+                        cmd = 'ok'
+                        client.send((cmd + '@').encode())
+                else:
+                    print('File not found')
+                    cmd = 'ok'
+                    client.send((cmd + '@').encode())
+        elif cmd == 'download' or cmd == '-d' or cmd == 'get':
+            try:
+                name = payload
+                if name == '*':
+                    client.send('ls'.encode())
+                    data = client.recv(1024).decode()
+                    cmd, msg = data.split("@")
+                    print("Downloading all files.")
+                    port = PORT
+                    for i in msg[1:-1].split('\n'):
+                        print(f"\n{'*' * 72}")
+                        t = CustomThread(target=recive_file,
+                                         args=(SELF_IP, port))
+                        t.start()
+                        client.send(f"get@{i}@{port}".encode())
+                        data = client.recv(1024).decode()
+                        cmd, msg = data.split("@")
+                        fname, file_hash = t.join()
+                        files = os.listdir(CLIENT_DATA_PATH)
+                        if fname in files:
+                            file_sha256 = sha256(open(
+                                CLIENT_DATA_PATH+slash+fname, 'rb').read()).hexdigest()
+                            if str(file_sha256) == file_hash:
+                                print(
+                                    f"sha256: {file_sha256}")
+                                print("File downloaded successfully.")
+                                print(f"{'*' * 72}\n")
+                            else:
+                                print("Downloaded file seems to be corrupted.")
+                                print(f"{'*' * 72}\n")
+                        else:
+                            print("Could not download file!")
+                            print(f"{'*' * 72}\n")
+                        port += 1
+                    client.send(cmd.encode())
+                else:
+                    print(f"\n{'*' * 72}")
+                    t = CustomThread(target=recive_file, args=(SELF_IP, PORT))
+                    t.start()
+                    client.send(f"{cmd}@{name}@{PORT}".encode())
+                    fname, file_hash = t.join()
+                    files = os.listdir(CLIENT_DATA_PATH)
+                    if fname in files:
+                        file_sha256 = sha256(open(
+                            CLIENT_DATA_PATH+slash+fname, 'rb').read()).hexdigest()
+                        if str(file_sha256) == file_hash:
+                            print(
+                                f"sha256: {file_sha256}")
+                            print("File downloaded successfully.")
+                            print(f"{'*' * 72}\n")
+                        else:
+                            print("Downloaded file seems to be corrupted.")
+                            print(f"{'*' * 72}\n")
+                    else:
+                        print("Could not download file!")
+                        print(f"{'*' * 72}\n")
+            except Exception as e:
+                print(e)
+                name = ''
+                client.send(f"{cmd}@{name}".encode())
+        else:
+            client.send(cmd.encode())
+
+    print("Disconnected from the server.")
+    client.close()
+
+
+if __name__ == "__main__":
+    while True:
+        ip = input("Enter <IP> to connect: ")
+        if re_ip.match(ip):
+            IP = ip
+            break
+        elif ip == '':
+            print("No ip address provided, using localhost by default")
+            ip = IP
+            break
+    main(IP)
---
+++ server.py
@@ -0,0 +1,262 @@
+import os
+import socket
+import threading
+import tqdm
+import platform
+from threading import Thread
+from hashlib import sha256
+
+
+class CustomThread(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args, **self._kwargs)
+
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+
+
+platform = platform.system()
+
+delete_file = 'rm'if platform != 'Windows' else 'del'
+slash = '/'if platform != 'Windows' else '\\'
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+IP = s.getsockname()[0]
+s.close()
+PORT = 4456
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+SERVER_DATA_PATH = "server_data"
+
+if os.path.isdir(SERVER_DATA_PATH) == False:
+    os.mkdir(SERVER_DATA_PATH)
+
+pwd = os.path.dirname(os.path.relpath(__name__)) + slash + SERVER_DATA_PATH
+
+help_text = """\n\npwd, cd: Show the current directory
+list, ls, dir, -l: List all the files from the server.
+upload, -u <path>: Upload a file to the server.
+download, get, -d <file to download>: Download file from the server.
+You can use '*' to download all the files: get *
+cat, type: to read he content of the file (if its readable)
+delete, rm, del  <filename>: Delete a file from the server.
+logout, quit, q, exit: Disconnect from the server.
+help, -h: List all the commands.\n"""
+
+
+def send_file(fname, ip, port, server_data_path=SERVER_DATA_PATH):
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((ip, int(port) + 1))
+        file = open(f"{server_data_path}{slash}{fname}", 'rb')
+        file_size = os.path.getsize(f"{server_data_path}{slash}{fname}")
+        file_hash = sha256(
+            open(f"{server_data_path}{slash}{fname}", 'rb').read()).hexdigest()
+        client.send(fname.split(slash)[-1].encode())
+        client.recv(1024).decode()
+        client.send(str(file_size).encode())
+        client.recv(1024).decode()
+        client.send(str(file_hash).encode())
+        client.recv(1024).decode()
+        data = file.read()
+        client.sendall(data)
+        client.send(b'<END>')
+        file.close()
+        client.close()
+    except Exception as e:
+        print(e)
+
+
+def recive_file(ip, port, server_data_path=SERVER_DATA_PATH):
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((ip, int(port) + 1))
+        server.listen()
+        client, addr = server.accept()
+        file_name = client.recv(1024).decode()
+        client.send('ok'.encode())
+        file_size = client.recv(1024).decode()
+        client.send('ok'.encode())
+        file_hash = client.recv(1024).decode()
+        client.send('ok'.encode())
+        print(
+            f'Reciving file: {file_name}  Size: {file_size}\nsha256: {file_hash}\n')
+        file = open(f'{server_data_path}{slash}' + file_name, 'wb')
+        file_bytes = b''
+        done = False
+        progress = tqdm.tqdm(unit='B', unit_scale=True,
+                             unit_divisor=1000, total=int(file_size))
+        while not done:
+            # 1048576, 1024, 8196, 65536, 1024000...
+            data = client.recv(int(1024000))
+            if file_bytes[-5:] == b'<END>':
+                done = True
+            else:
+                file_bytes += data
+            progress.update(len(data))
+        file.write(file_bytes.replace(b'<END>', b''))
+        file.close()
+        client.close()
+        server.close()
+        return file_name, file_hash
+    except Exception as e:
+        print(e)
+
+
+def handle_client(conn, addr):
+    client_platform = conn.recv(SIZE).decode()
+    print(f"[NEW CONNECTION] {addr} connected [PLATFORM] {client_platform}")
+    conn.send(("OK@Welcome to the File Server:" + help_text).encode())
+
+    while True:
+        try:
+            data = conn.recv(SIZE).decode()
+            data = data.split("@")
+            cmd = data[0]
+        except:
+            break
+        if cmd == "list" or cmd == "-l" or cmd == "ls" or cmd == 'dir':
+            files = os.listdir(SERVER_DATA_PATH)
+            send_data = "OK@"
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                send_data += "\n" + "\n".join(f for f in files) + "\n"
+            conn.send(send_data.encode())
+        elif cmd == 'OK':
+            send_data = "OK@"
+            conn.send(send_data.encode())
+        elif cmd == 'pwd' or cmd == 'cd':
+            send_data = 'OK@'
+            conn.send(f'{send_data}{pwd}'.encode())
+        elif cmd == "upload" or cmd == "-u":
+            print(f"\n{'*' * 72}")
+            print(f"{addr} has requested a file upload through port: {int(PORT) + 1}")
+            t = CustomThread(target=recive_file, args=(IP, PORT))
+            t.start()
+            fname, file_hash = t.join()
+            files = os.listdir(SERVER_DATA_PATH)
+            if fname in files:
+                file_sha256 = sha256(open(
+                    SERVER_DATA_PATH+slash+fname, 'rb').read()).hexdigest()
+                if str(file_sha256) == file_hash:
+                    print(f"sha256: {file_sha256}")
+                    conn.send("OK@File uploaded successfully.".encode())
+                else:
+                    conn.send("OK@Uploaded file seems to be corrupted".encode())
+                    os.system(
+                        f"{delete_file} {SERVER_DATA_PATH}{slash}{fname}")
+            else:
+                conn.send("OK@Could not upload file!".encode())
+            print(f"{'*' * 72}\n")
+        elif cmd == 'download' or cmd == '-d' or cmd == 'get':
+            files = os.listdir(SERVER_DATA_PATH)
+            send_data = "OK@"
+            try:
+                fname = data[1].split(slash)[-1]
+                port = data[2]
+                print(
+                    f"{addr} has requested a file dowload: {fname} through port: {int(port) + 1}")
+            except:
+                fname = ''
+                send_data += "You must specify the file to download."
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                try:
+                    if fname in files or fname != '':
+                        try:
+                            threading.Thread(target=send_file,
+                                             args=(fname, addr[0], port)).start()
+                        except Exception as e:
+                            print(f"ERROR: {e}")
+                            send_data += "File could not be downloaded!"
+                    elif fname != '':
+                        send_data += "File not found."
+                except Exception as e:
+                    print(e)
+            conn.send(send_data.encode())
+        elif cmd == "delete" or cmd == "rm" or cmd == "del":
+            files = os.listdir(SERVER_DATA_PATH)
+            send_data = "OK@"
+            try:
+                filename = data[1].split(slash)[-1]
+                print(f"{addr} has requested the deletion of the file: {filename}")
+            except:
+                filename = ''
+                send_data += "You must specify the file to delete."
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                if filename in files or filename != '':
+                    os.system(
+                        f'{delete_file} "{SERVER_DATA_PATH}{slash}{filename}"')
+                    files = os.listdir(SERVER_DATA_PATH)
+                    if filename not in files:
+                        send_data += "File deleted successfully."
+                        print(f"{filename} deleted successfully")
+                    else:
+                        send_data += "File could not be deleted!"
+                elif filename != '':
+                    send_data += "File not found."
+            conn.send(send_data.encode())
+        elif cmd == "cat" or cmd == "type":
+            files = os.listdir(SERVER_DATA_PATH)
+            send_data = 'OK@'
+            try:
+                filename = data[1].split(slash)[-1]
+                print(f"{addr} has requested the reading of the file: {filename}")
+            except:
+                filename = ''
+                send_data += "You must specify the file to read."
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                if filename in files:
+                    try:
+                        f = open(filename, 'r')
+                        content = ('#' * 79) + '\n' + ('-' * (15 + len(filename))) + '\n| File name: ' + filename + ' |\n' + ('-' * (15 + len(filename))) + '\n\n' + \
+                            ''.join(f.readlines()) + '\n' + ('#' * 79)
+                        send_data += content
+                    except Exception as e:
+                        send_data += "File could not be read!"
+                elif filename != '':
+                    send_data += "File not found."
+            conn.send(send_data.encode())
+        elif cmd == "logout" or cmd == "exit" or cmd == "quit" or cmd == "q":
+            break
+        elif cmd == "help" or cmd == '-h':
+            data = "OK@"
+            data += help_text
+            conn.send(data.encode())
+        else:
+            data = "OK@You have to chose one available command:"
+            data += help_text
+            conn.send(data.encode())
+    print(f"[DISCONNECTED] {addr} disconnected")
+    conn.close()
+
+
+def main():
+    print(f"\n[STARTING] Server is starting... [PLATFORM] {platform}\n")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print(f"[LISTENING] Server is listening on {IP}:{PORT}.")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+if __name__ == "__main__":
+    main()
